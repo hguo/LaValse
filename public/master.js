ec98ea6ef7f9093ecfe55abff6c19d115f482785
Hanqi Guo <hguo@anl.gov>
2016-12-11 19:16:33 -0600

    adding machine view

--- public/master.js
+++ public/master.js
@@ -4,6 +4,7 @@ var query = {
   tg: 86400000 // aggregation by day
 };
 var severityChart, componentChart, categoryChart, locationTypeChart, timeVolumeChart;
+var machineView;
 
 init();
 
@@ -15,16 +16,17 @@ function init() {
         {L: 0, T: 0, W: 200, H: 100});
     componentChart = new barChart(
         "component", "#componentChart", histogramToArray(d.component),
-        {L: 0, T: 100, W: 200, H: 300});
+        {L: 0, T: 100, W: 200, H: 200});
     categoryChart = new barChart(
         "category", "#categoryChart", histogramToArray(d.category),
-        {L: 200, T: 0, W: 200, H: 400});
+        {L: 200, T: 0, W: 200, H: 300});
     locationTypeChart = new barChart(
         "locationType", "#locationTypeChart", histogramToArray(d.locationType),
-        {L: 400, T: 0, W: 200, H: 400});
+        {L: 400, T: 0, W: 200, H: 300});
     timeVolumeChart = new timeVolumeChart(
         "#timeVolumeChart", histogramToArray(d.timeVolume), 
-        {L: 0, T: 400, W: 600, H: 100});
+        {L: 0, T: 300, W: 1200, H: 100});
+    machineView = new machineView();
   });
 }
 
