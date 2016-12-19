2c8e1b5dc39daa77bfbd9dc106e28b1b13ee9d18
Hanqi Guo <hguo@anl.gov>
2016-12-18 20:17:28 -0600

    WIP: record locations for each slot

--- cpp/rasQuery.h
+++ cpp/rasQuery.h
@@ -39,8 +39,9 @@ struct QueryResults {
   uint32_t nmatched = 0;
   uint32_t msgID[NUM_MSGID], component[NUM_COMP], locationType[NUM_LOCTYPE], 
            category[NUM_CAT], severity[NUM_SEV], controlAction[NUM_CTLACT];
-  uint32_t location[MAX_NUM_LOC];
-  uint32_t *timeVolumes;
+  uint32_t *location, *locationRecID; 
+  uint32_t *timeVolumes, *timeVolumesRecID;
+
   std::vector<uint32_t> topRecIDs;
 
   explicit QueryResults(const Query& q);
@@ -89,13 +90,24 @@ struct Query {
     }
   }
 
-  inline void add1(uint32_t& a) {
+  inline uint32_t add1(uint32_t& a) {
     // __sync_fetch_and_add(&a, 1);
-    a ++;
+    return ++ a;
   }
-        
+  
+  inline void add1(uint32_t& a, uint32_t* recIDs, uint32_t msgID) {
+    uint32_t i = add1(a);
+    recIDs[i%MAX_EVENTS_PER_SLOT] = msgID;
+  }
+
   inline void add1(uint32_t* timeVolumes, int nslots, int index, int t) {
     add1(timeVolumes[nslots*index+t]);
+  }
+  
+  inline void add1(uint32_t* timeVolumes, uint32_t* timeVolumesRecID, int nslots, int index, int t, int msgID) {
+    uint32_t i = add1(timeVolumes[nslots*index+t]);
+    // if (i<MAX_EVENTS_PER_SLOT) timeVolumesRecID[(nslots*index+t)*MAX_EVENTS_PER_SLOT + i] = msgID;
+    timeVolumesRecID[(nslots*index+t)*MAX_EVENTS_PER_SLOT + i%MAX_EVENTS_PER_SLOT] = msgID;
   }
 
   void crossfilter_thread(int nthreads, int tid, const std::vector<Event>& events, QueryResults& results) {
@@ -112,14 +124,22 @@ struct Query {
       const Event& e = events[i];
       crossfilter_kernel(e, b, c);
       if (c[0]) {
-        uint32_t t = e.aggregateTime(T0, tg); // FIXME
+        uint32_t t = e.aggregateTime(T0, tg); 
         switch (volumeBy) {
+#if 0
         case VAR_NONE: add1(results.timeVolumes[t]); break;
         case VAR_MSGID: add1(results.timeVolumes, nslots, e.msgID, t); break;
         case VAR_COMP: add1(results.timeVolumes, nslots, e.component(), t); break;
         case VAR_LOC: add1(results.timeVolumes, nslots, e.locationType, t); break;
         case VAR_CAT: add1(results.timeVolumes, nslots, e.category(), t); break;
         case VAR_SEV: add1(results.timeVolumes, nslots, e.severity(), t); break;
+#endif
+        case VAR_NONE: add1(results.timeVolumes[t], results.timeVolumesRecID, e.msgID); break;
+        case VAR_MSGID: add1(results.timeVolumes, results.timeVolumesRecID, nslots, e.msgID, t, e.msgID); break;
+        case VAR_COMP: add1(results.timeVolumes, results.timeVolumesRecID, nslots, e.component(), t, e.msgID); break;
+        case VAR_LOC: add1(results.timeVolumes, results.timeVolumesRecID, nslots, e.locationType, t, e.msgID); break;
+        case VAR_CAT: add1(results.timeVolumes, results.timeVolumesRecID, nslots, e.category(), t, e.msgID); break;
+        case VAR_SEV: add1(results.timeVolumes, results.timeVolumesRecID, nslots, e.severity(), t, e.msgID); break;
         default: break;
         }
       }
@@ -207,26 +227,33 @@ struct Query {
 
 QueryResults::QueryResults(const Query& q)
 {
+  nslots = (q.T1 - q.T0) / q.tg;
+  nvolumes = ras::nvolumes[q.volumeBy];
+  
+  timeVolumes = (uint32_t*)malloc(nslots*nvolumes*4);
+  timeVolumesRecID = (uint32_t*)malloc(nslots*nvolumes*MAX_EVENTS_PER_SLOT*4);
+  memset(timeVolumes, 0, nslots*nvolumes*4);
+  
+  location = (uint32_t*)malloc(nlocations[q.LOD]*4);
+  locationRecID = (uint32_t*)malloc(nlocations[q.LOD]*MAX_EVENTS_PER_SLOT*4);
+  memset(location, 0, nlocations[q.LOD]*4);
+
   memset(msgID, 0, NUM_MSGID*4);
   memset(component, 0, NUM_COMP*4);
   memset(locationType, 0, NUM_LOCTYPE*4);
   memset(category, 0, NUM_CAT*4);
   memset(severity, 0, NUM_SEV*4);
   memset(controlAction, 0, NUM_CTLACT*4);
-  memset(location, 0, MAX_NUM_LOC*4);
   
-  nslots = (q.T1 - q.T0) / q.tg;
-  nvolumes = ras::nvolumes[q.volumeBy];
-
-  timeVolumes = (uint32_t*)malloc(nslots*nvolumes*4);
-  memset(timeVolumes, 0, nslots*nvolumes*4);
-
   topRecIDs.reserve(q.top);
 }
 
 QueryResults::~QueryResults()
 {
   free(timeVolumes);
+  free(timeVolumesRecID);
+  free(location);
+  free(locationRecID);
 }
 
 }
