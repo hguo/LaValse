cb3eda52dd6f0f97feea88883c4ec67642e5dbe2
Hanqi Guo <hguo@anl.gov>
2017-01-25 16:07:08 -0600

    WIP: proj profiles

---
+++ buildProjInfo.js
@@ -0,0 +1,38 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb://localhost:27017/catalog";
+
+const colors = ["", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+var str = "🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🥝 🥑 🍅 🍆 🥒 🥕 🌽 🌶 🥔 🍠 🌰 🥜 🍯 🥐 🍞 🥖 🧀 🥚 🍳 🥓 🥞 🍤 🍗 🍖 🍕 🌭 🍔 🍟 🥙 🌮 🌯 🥗 🥘 🍝 🍜 🍲 🍥 🍣 🍱 🍛 🍚 🍙 🍘 🍢 🍡 🍧 🍨 🍦 🍰 🎂 🍮 🍭 🍬 🍫 🍿 🍩 🍪 🥛 🍼 ☕️ 🍵 🍶 🍺 🍻 🥂 🍷 🥃 🍸 🍹 🍾 🥄 🍴 🍽";
+var emojis = str.split(" ");
+
+var aliases = [];
+for (var i=0; i<colors.length; i++) {
+  for (var j=0; j<emojis.length; j++) {
+    aliases.push([colors[i], emojis[j]]);
+  }
+}
+
+console.log(aliases);
+
+MongoClient.connect(uri, function(err, db) {
+  buildUserInfo(db, function() {
+    db.close();
+  });
+});
+
+var buildUserInfo = function(db, cb) {
+  var collection = db.collection("cobalt");
+  collection.aggregate([
+        {
+          $group: {"_id": "$cobaltProjectName", count: {$sum: 1}}
+          // $group: {"_id": "$cobaltUserName", count: {$sum: 1}}
+          // $group: {"_id": "$machinePartition", count: {$sum: 1}}
+        }
+    ])
+    .toArray(function(err, docs) {
+      for (var i=0; i<docs.length; i++) {
+        console.log('"' + docs[i]._id + '","' + aliases[i][0] + '","' +  aliases[i][1] + '"');
+      }
+    });
+  cb();
+}
---
+++ buildUserInfo.js
@@ -0,0 +1,36 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb://localhost:27017/catalog";
+
+const colors = ["", "#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6"];
+var str = "🐶 🐱 🐭 🐹 🐰 🦊 🐻 🐼 🐨 🐯 🦁 🐮 🐷 🐽 🐸 🐒 🐔 🐧 🐦 🐤 🐣 🐥 🦆 🦅 🦉 🦇 🐺 🐗 🐴 🦄 🐝 🐛 🦋 🐌 🐚 🐞 🐜 🕷 🕸 🐢 🐍 🦎 🦂 🦀 🦑 🐙 🦐 🐠 🐟 🐡 🐬 🦈 🐳 🐋 🐊 🐆 🐅 🐃 🐂 🐄 🦌 🐪 🐫 🐘 🦏 🦍 🐎 🐖 🐐 🐏 🐑 🐕 🐩 🐈 🐓 🦃 🕊 🐇 🐁 🐀 🐿 🐉 🐲";
+var emojis = str.split(" ");
+
+var aliases = [];
+for (var i=0; i<colors.length; i++) {
+  for (var j=0; j<emojis.length; j++) {
+    aliases.push([colors[i], emojis[j]]);
+  }
+}
+
+MongoClient.connect(uri, function(err, db) {
+  buildUserInfo(db, function() {
+    db.close();
+  });
+});
+
+var buildUserInfo = function(db, cb) {
+  var collection = db.collection("cobalt");
+  collection.aggregate([
+        {
+          // $group: {"_id": "$cobaltProjectName", count: {$sum: 1}}
+          $group: {"_id": "$cobaltUserName", count: {$sum: 1}}
+          // $group: {"_id": "$machinePartition", count: {$sum: 1}}
+        }
+    ])
+    .toArray(function(err, docs) {
+      for (var i=0; i<docs.length; i++) {
+        console.log('"' + docs[i]._id + '","' + aliases[i][0] + '","' +  aliases[i][1] + '"');
+      }
+    });
+  cb();
+}
