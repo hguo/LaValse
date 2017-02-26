f2f0993a8e371dee8ff8caf2f3085544c6f751f8
Hanqi Guo <hguo@anl.gov>
2017-02-25 21:17:16 -0600

    WIP: analysis

--- analysis.js
+++ analysis.js
@@ -1,14 +1,34 @@
 const ras = require("./rasbook");
 const jstat = require("jstat").jStat;
 
-
 function temporalCorrelation(timeVolumesByMsgID) {
-  console.log(timeVolumesByMsgID.length);
-  var data = timeVolumesByMsgID.filter(function(d) {
-    console.log(jstat.covariance(d, d));
-    var sum = d.reduce(function(acc, val) {return acc + val;});
-    return sum != 0;
-  });
+  var data = {};
+  var results = {};
+
+  for (var i=0; i<timeVolumesByMsgID.length; i++) {
+    var volume = timeVolumesByMsgID[i];
+    var sum = volume.reduce(function(acc, val) {return acc + val;});
+    if (sum != 0) {
+      var msgID = ras.eventMap.val(i);
+      
+      // for (var j=0; j<volume.length; j++) {
+        // volume[j] = volume[j] == 0 ? 0 : 1;
+      // }
+      data[msgID] = volume;
+    }
+  }
+  
+  for (var msgID in data) {
+    var volume = data[msgID];
+    for (var msgID1 in data) {
+      if (msgID == msgID1) continue; 
+      var volume1 = data[msgID1];
+      var coef = jstat.corrcoeff(volume, volume1);
+      if (coef > 0.8) {
+        console.log(msgID, msgID1, jstat.corrcoeff(volume, volume1));
+      }
+    }
+  }
 }
 
 module.exports = {
